75bb5bf7eb22c3f9a6d5849351c62f0f7ac56aa0
adityanandanx <nandanaditya985@gmail.com>
2025-02-14 13:24:14 +0530

    feat: door delay and timer

--- entities/door/door.gd
+++ entities/door/door.gd
@@ -1,31 +1,50 @@
 class_name Door
 extends Activatable
 
-
 @export var is_interactable: bool = true
 @export var is_open: bool = false
+@export var auto_close: bool = true
+@export_range(0, 10, .05, "suffix:s") var opening_time: float = 0.25
+@export_range(0, 10, .05, "suffix:s") var close_delay: float = 0.0
+
+@export var show_debug_label: bool = false
 
 @onready var ap: AnimationPlayer = $AnimationPlayer
 @onready var interactable_area: InteractableArea = $InteractableArea
+@onready var close_delay_timer: Timer = $CloseDelayTimer
+@onready var close_delay_timer_label: Label3D = $CloseDelayTimerLabel
 
 func _ready() -> void:
 	interactable_area.enabled = is_interactable
+	ap.speed_scale = 1 / opening_time
 	if is_open:
 		open()
 	else:
 		close()
+	if show_debug_label:
+		close_delay_timer_label.show()
+	else:
+		close_delay_timer_label.hide()
+
+func _process(delta: float) -> void:
+	if show_debug_label:
+		close_delay_timer_label.text = "%.2f" % close_delay_timer.time_left + "s"
 
 func on_activate() -> void:
+	close_delay_timer.stop()
 	open()
 
 func on_deactivate() -> void:
-	close()
+	if auto_close:
+		close_delay_timer.start(close_delay)
 
 func open() -> void:
+	if is_open: return
 	ap.play("open")
 	is_open = true
 
 func close() -> void:
+	if not is_open: return
 	ap.play_backwards("open")
 	is_open = false
 	
@@ -34,3 +53,7 @@ func _on_interactable_area_interacted() -> void:
 		close()
 	else:
 		open()
+
+
+func _on_close_delay_timer_timeout() -> void:
+	close()
