b93fef2c040fadf0b97e1e66b9228df28ed2802e
adityanandanx <nandanaditya985@gmail.com>
2025-02-16 17:31:39 +0530

    feat: player debug mode

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,7 +1,6 @@
 class_name Player
 extends CharacterBody3D
 
-
 @export var WALK_SPEED = 4.0
 @export var SPRINT_SPEED = 8.0
 @export var STAMINA_DELTA = .5
@@ -13,6 +12,10 @@ extends CharacterBody3D
 #fov variables
 @export var BASE_FOV = 75.
 @export var FOV_CHANGE = 1.5
+# Debug mode variables
+@export var DEBUG_FLY_SPEED = 10.0
+var debug_mode := false
+var original_collision_mask: int
 
 var health := 100.0
 var stamina := 100.0:
@@ -28,14 +31,16 @@ var stamina := 100.0:
 @onready var narration_stream: AudioStreamPlayer = %NarrationStream
 @onready var footsteps_stream: AudioStreamPlayer = %FootstepsStream
 
-
 var t_bob := 0.0
 var speed := 0.0
 var time_factor := 1.0
+var narrations: Array[AudioStream] = []
 
 func _ready() -> void:
 	Global.player = self
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	# Store original collision mask for toggling noclip
+	original_collision_mask = collision_mask
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -43,9 +48,61 @@ func _unhandled_input(event: InputEvent) -> void:
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-70), deg_to_rad(60))
 	
+	# Toggle debug mode with Tab key
+	if event.is_action_pressed("toggle_debug"):
+		toggle_debug_mode()
+		
+	if event.is_action_pressed("escape"):
+		match Input.mouse_mode:
+			Input.MOUSE_MODE_VISIBLE:
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			Input.MOUSE_MODE_CAPTURED:
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+func toggle_debug_mode() -> void:
+	debug_mode = !debug_mode
+	if debug_mode:
+		# Enable noclip by setting collision mask to 0
+		collision_mask = 0
+		print("Debug mode enabled")
+	else:
+		# Restore original collision mask
+		collision_mask = original_collision_mask
+		# Reset vertical velocity when exiting debug mode
+		velocity.y = 0
+		print("Debug mode disabled")
+	
 func _physics_process(delta: float) -> void:
 	time_factor = 1/Engine.time_scale
 	delta *= time_factor
+	
+	if debug_mode:
+		_handle_debug_movement(delta)
+	else:
+		_handle_normal_movement(delta)
+	
+	move_and_slide()
+	if !debug_mode:
+		_push_pushables(delta)
+
+func _handle_debug_movement(delta: float) -> void:
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Vertical movement in debug mode
+	if Input.is_action_pressed("jump"):
+		direction.y = 1.0
+	if Input.is_action_pressed("sprint"):  # You'll need to add this action in the input map
+		direction.y = -1.0
+	
+	# Apply movement in all directions
+	velocity = direction * DEBUG_FLY_SPEED * time_factor
+	
+	# Keep camera effects disabled in debug mode
+	camera.transform.origin = Vector3.ZERO
+	camera.fov = BASE_FOV
+
+func _handle_normal_movement(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta * time_factor
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -71,6 +128,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+	
 	# Head bob
 	t_bob += delta/time_factor * velocity.length() * float(is_on_floor()) + .025
 	camera.transform.origin = _headbob(t_bob)
@@ -81,9 +139,8 @@ func _physics_process(delta: float) -> void:
 		speed_indicator.speed_up()
 	else:
 		speed_indicator.slow_down()
-	move_and_slide()
-	_push_pushables(delta)
-	
+
+# Rest of the original functions remain unchanged
 func _headbob(time: float) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
@@ -115,7 +172,6 @@ func _on_update_stamina(new: float):
 func scare(trauma: float = 10.0):
 	%ShakeableCameraHolder.add_trauma(trauma)
 
-var narrations: Array[AudioStream] = []
 func say(audio_stream: AudioStream, subtitle: String):
 	narrations.push_back(audio_stream)
 	while len(narrations):
