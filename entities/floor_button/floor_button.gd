7559d93b7d28f272824a0ebe275d37845a85b90d
adityanandanx <nandanaditya985@gmail.com>
2025-02-17 01:15:28 +0530

    fix: button

--- entities/floor_button/floor_button.gd
+++ entities/floor_button/floor_button.gd
@@ -2,6 +2,8 @@ class_name FloorButton
 extends Activator
 @onready var ap: AnimationPlayer = $AnimationPlayer
 
+var bodies = []
+
 func after_activate() -> void:
 	ap.play("press")
 
@@ -10,16 +12,24 @@ func after_deactivate() -> void:
 
 
 func _on_button_area_area_entered(area: Area3D) -> void:
+	if area in bodies: return
+	bodies.push_back(area)
 	activate()
 
 
 func _on_button_area_area_exited(area: Area3D) -> void:
-	deactivate()
+	bodies.erase(area)
+	if len(bodies) == 0:
+		deactivate()
 
 
 func _on_button_area_body_entered(body: Node3D) -> void:
+	if body is StaticBody3D or body in bodies: return
+	bodies.push_back(body)
 	activate()
 
 
 func _on_button_area_body_exited(body: Node3D) -> void:
-	deactivate()
+	bodies.erase(body)
+	if len(bodies) == 0:
+		deactivate()
